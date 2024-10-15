c133f8f64bc994e9b7874af6940f583f0f0b4cbb
karam zuhd <karamzohud2@gmail.com>
2024-10-15 17:06:02 +0300

    domain update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,7 @@
 export const ARTICLE_PER_PAGE = 6;
-export const DOMAIN="http://localhost:3000";
-// const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
-// const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = "https://cloud-hosting-next.vercel.app";
+const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
-// export const DOMAIN = process.env.NODE_ENV === 'production' 
-//     ? PRODUCTION_DOMAIN
-//     : DEVELOPMENT_DOMAIN;
+export const DOMAIN = process.env.NODE_ENV === 'production' 
+    ? PRODUCTION_DOMAIN
+    : DEVELOPMENT_DOMAIN;
